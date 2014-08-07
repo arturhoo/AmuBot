a8137b0496fee75cf98879bb87946957afb78f09
Artur Rodrigues <arturhoo@gmail.com>
2014-08-06 21:23:26 -0300

    Add video thumbnail

--- videos.rb
+++ videos.rb
@@ -16,14 +16,29 @@ class Videos
     links = RedditKit.links('videos', category: 'hot')
 
     links.each do |l|
-      if redis.get(l.id).nil? && l.score >= 1000
+      if redis.get(l.id).nil? && l.score >= 1500 && !l.is_self?
         redis.set l.id, 'check'
 
         text = "<strong>Video:</strong> <a href='#{l.url}'>#{l.title}</a>"
+
+        begin
+          thumbnail_url = thumbnail(l)
+          text += "<br><a href='#{l.url}'><img src='#{thumbnail_url}' "\
+                  'height=160px></a>'
+        rescue
+        end
         hipchat[$room].send('Videos', text, message_format: 'html',
                             color: 'purple')
         break
       end
     end
+  end
+
+  def self.thumbnail(link)
+    return unless link.media_embed && link.media_embed[:content]
+    content = link.media_embed[:content]
+    regex = /.+image=(.+hqdefault.jpg)&amp.+/
+    encoded_url = regex.match(content).captures.first
+    URI.decode(encoded_url)
   end
 end
